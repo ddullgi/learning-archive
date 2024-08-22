45ecd16466124ab76c2589c914bb4f0c2b781c05
황상윤 <s_infinite@naver.com>
2024-08-22 23:54:56 +0900

    [feat] section5. 커스텀 커맨드와 공통 훅

--- test/실전 프론트엔드 테스트 시작하기(by 캡틴판교)/cypress/e2e/counter.cy.js
+++ test/실전 프론트엔드 테스트 시작하기(by 캡틴판교)/cypress/e2e/counter.cy.js
@@ -1,22 +1,24 @@
 // E2E 테스트 작성은 코드 관점이 아닌 사용자 관점으로 작성해야된다.
 describe("카운터 앱", () => {
+  beforeEach(() => {
+    // 현재 그룹핑된 describe 안에서 모든 테스트 코드가 실행되기 전에 공통 로직을 실행하고 싶다면 여기에 추가하세요.
+    cy.visit("http://localhost:3000");
+  });
+
   // 첫 번째 테스트 시나리오
   it("페이지에 진입하면 카운터 앱이 정상적으로 실행된다(0이 표시된다)", () => {
-    cy.visit("http://localhost:3000");
-    cy.get("[data-cy=counter]").contains(0);
+    cy.getByCy("counter").contains(0);
   });
 
   // 두 번째 테스트 시나리오
   it("+ 버튼을 누르면 카운터 값이 1 증가한다", () => {
-    cy.visit("http://localhost:3000");
-    cy.get("[data-cy=add-button]").click();
-    cy.get("[data-cy=counter]").contains(1);
+    cy.getByCy("add-button").click();
+    cy.getByCy("counter").contains(1);
   });
 
   // 세 번째 테스트 시나리오
   it("- 버튼을 누르면 카운터 값이 1 감소한다", () => {
-    cy.visit("http://localhost:3000");
-    cy.get("[data-cy=minus-button]").click();
-    cy.get("[data-cy=counter]").contains(-1);
+    cy.getByCy("minus-button").click();
+    cy.getByCy("counter").contains(-1);
   });
 });
