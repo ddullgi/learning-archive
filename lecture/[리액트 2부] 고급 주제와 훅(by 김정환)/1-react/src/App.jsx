8ac84e21d92fdbd8363f6ff65f83fa096b5a3a9f
황상윤 <s_infinite@naver.com>
2025-01-12 22:56:00 +0900

    [feat] 고차 컴포넌트 학습

--- lecture/[리액트 2부] 고급 주제와 훅(by 김정환)/1-react/src/App.jsx
+++ lecture/[리액트 2부] 고급 주제와 훅(by 김정환)/1-react/src/App.jsx
@@ -1,4 +1,5 @@
 import * as MyRouter from "./lib/MyRouter";
+import { getComponentName } from "./lib/utils";
 import CartPage from "./pages/CartPage";
 import OrderPage from "./pages/OrderPage";
 import ProductPage from "./pages/ProductPage";
@@ -13,101 +14,144 @@ const App = () => (
   </MyRouter.Router>
 );
 
-export default App;
+// export default App;
 
 //? ref 학습
-// import React from "react";
-// class MyComponent extends React.Component {
-//   divRef = React.createRef();
-
-//   constructor(props) {
-//     super(props);
-//     console.log("constructor", this.divRef);
-//   }
-
-//   render() {
-//     return <div ref={this.divRef}>div</div>;
-//   }
-
-//   componentDidMount() {
-//     console.log("componentDidMount", this.divRef);
-//     const divElement = this.divRef.current;
-//     divElement.style.backgroundColor = "red";
-//     divElement.style.height = "100px";
-//     divElement.style.width = "100px";
-//   }
-// }
-
-// export default MyComponent;
+{
+  // import React from "react";
+  // class MyComponent extends React.Component {
+  //   divRef = React.createRef();
+  //   constructor(props) {
+  //     super(props);
+  //     console.log("constructor", this.divRef);
+  //   }
+  //   render() {
+  //     return <div ref={this.divRef}>div</div>;
+  //   }
+  //   componentDidMount() {
+  //     console.log("componentDidMount", this.divRef);
+  //     const divElement = this.divRef.current;
+  //     divElement.style.backgroundColor = "red";
+  //     divElement.style.height = "100px";
+  //     divElement.style.width = "100px";
+  //   }
+  // }
+  // export default MyComponent;
+}
 
 //? 이벤트 에미터 학습
-// import createEventEmitter from "shared/lib/EventEmitter";
-
-// const eventEmitter = createEventEmitter(0);
-// const loger = (value) => console.log(value);
-
-// eventEmitter.on(loger);
-// console.log(eventEmitter.get());
-// eventEmitter.set(1);
-// eventEmitter.set(2);
-
-// setTimeout(() => eventEmitter.set(10), 3000);
+{
+  // import createEventEmitter from "shared/lib/EventEmitter";
+  // const eventEmitter = createEventEmitter(0);
+  // const loger = (value) => console.log(value);
+  // eventEmitter.on(loger);
+  // console.log(eventEmitter.get());
+  // eventEmitter.set(1);
+  // eventEmitter.set(2);
+  // setTimeout(() => eventEmitter.set(10), 3000);
+}
 
 //? 컨텍스트 학습
+{
+  // import MyReact from "./lib/MyReact";
+  // import React from "react";
+  // const counterContext = MyReact.createContext({
+  //   count: 0,
+  //   setCount: () => {},
+  // });
+  // class CountProvider extends React.Component {
+  //   constructor(props) {
+  //     super(props);
+  //     this.state = {
+  //       count: 0,
+  //     };
+  //   }
+  //   render() {
+  //     const value = {
+  //       count: this.state.count,
+  //       setCount: (nextValue) => this.setState({ count: nextValue }),
+  //     };
+  //     return (
+  //       <counterContext.Provider value={value}>
+  //         {this.props.children}
+  //       </counterContext.Provider>
+  //     );
+  //   }
+  // }
+  // const Count = () => {
+  //   return (
+  //     <counterContext.Consumer>
+  //       {(value) => <div>{value.count}</div>}
+  //     </counterContext.Consumer>
+  //   );
+  // };
+  // const PlussButton = () => {
+  //   return (
+  //     <counterContext.Consumer>
+  //       {(value) => (
+  //         <button onClick={() => value.setCount(value.count + 1)}>
+  //           + 카운트 올리기
+  //         </button>
+  //       )}
+  //     </counterContext.Consumer>
+  //   );
+  // };
+  // export default () => (
+  //   <CountProvider>
+  //     <Count />
+  //     <PlussButton />
+  //   </CountProvider>
+  // );
+}
 
-// import MyReact from "./lib/MyReact";
-// import React from "react";
+//? 고차 컴포넌트 학습
+import React from "react";
 
-// const counterContext = MyReact.createContext({
-//   count: 0,
-//   setCount: () => {},
-// });
+class Header extends React.Component {
+  render() {
+    return <header>Header</header>;
+  }
+}
 
-// class CountProvider extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       count: 0,
-//     };
-//   }
+class Button extends React.Component {
+  handleClick = () => {
+    this.props.log("클릭");
+  };
 
-//   render() {
-//     const value = {
-//       count: this.state.count,
-//       setCount: (nextValue) => this.setState({ count: nextValue }),
-//     };
+  render() {
+    return <button onClick={this.handleClick}>버튼</button>;
+  }
+}
 
-//     return (
-//       <counterContext.Provider value={value}>
-//         {this.props.children}
-//       </counterContext.Provider>
-//     );
-//   }
-// }
+const withLogging = (WrappedCompent) => {
+  function log(message) {
+    console.log(`[${getComponentName(WrappedCompent)}] ${message}`);
+  }
 
-// const Count = () => {
-//   return (
-//     <counterContext.Consumer>
-//       {(value) => <div>{value.count}</div>}
-//     </counterContext.Consumer>
-//   );
-// };
+  class WithLoging extends React.Component {
+    componentDidMount() {
+      log("마운트");
+    }
 
-// const PlussButton = () => {
-//   return (
-//     <counterContext.Consumer>
-//       {(value) => (
-//         <button onClick={() => value.setCount(value.count + 1)}>
-//           + 카운트 올리기
-//         </button>
-//       )}
-//     </counterContext.Consumer>
-//   );
-// };
+    render() {
+      const enhancedProps = {
+        log,
+      };
+      return <WrappedCompent {...this.props} {...enhancedProps} />;
+    }
+  }
 
-// export default () => (
-//   <CountProvider>
-//     <Count />
-//     <PlussButton />
-//   </CountProvider>
-// );
+  return WithLoging;
+};
+
+const EnhancedHeader = withLogging(Header);
+const EnhancedButton = withLogging(Button);
+
+export default () => {
+  return (
+    <div>
+      <EnhancedHeader />
+      <EnhancedButton />
+    </div>
+  );
+};
