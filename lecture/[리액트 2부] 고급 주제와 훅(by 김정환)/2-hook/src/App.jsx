af4d0f49b88457d462daac7851b12dac7f2c0942
황상윤 <s_infinite@naver.com>
2025-01-22 22:55:42 +0900

    [feat] productPage 함수형 적용

--- lecture/[리액트 2부] 고급 주제와 훅(by 김정환)/2-hook/src/App.jsx
+++ lecture/[리액트 2부] 고급 주제와 훅(by 김정환)/2-hook/src/App.jsx
@@ -1,6 +1,10 @@
-// const App = () => <>2-hook</>;
+import ProductPage from "./pages/ProductPage";
 
-// export default App;
+const App = () => {
+  return <ProductPage />;
+};
+
+export default App;
 
 //? 함수 클래스 비교
 {
@@ -68,63 +72,54 @@
   // export default () => <NameField />;
 }
 
-import React from "react";
-import MyReact from "./lib/MyReact";
-
-const Counter = () => {
-  MyReact.resetCursor();
-
-  const [count, setCount] = React.useState(0);
-  const [name, setName] = React.useState("");
-
-  const handleClick = () => setCount(count + 1);
-
-  const handleChangeName = (e) => {
-    setName(e.target.value);
-  };
-
-  MyReact.useEffect(() => {
-    document.title = `count: ${count} | name: ${name}`;
-    console.log("effect1");
-
-    return function cleanup() {
-      document.title = "";
-      console.log("effect1 cleanup");
-    };
-  }, [count, name]);
-
-  MyReact.useEffect(() => {
-    localStorage.setItem("name", name);
-    console.log("effect2");
-  }, [name]);
-
-  MyReact.useEffect(() => {
-    setName(localStorage.getItem("name") || "");
-    console.log("effect3");
-  }, []);
-
-  console.log("Counter rendered");
-
-  return (
-    <>
-      <button onClick={handleClick}>더하기</button>
-      <input value={name} onChange={handleChangeName} />
-    </>
-  );
-};
-
-export default () => {
-  const [mounted, setMounted] = React.useState(false);
-  const handleToggle = () => {
-    const nextMounted = !mounted;
-    if (!nextMounted) MyReact.cleanupEffects();
-    setMounted(nextMounted);
-  };
-
-  return (
-    <>
-      <button onClick={handleToggle}>컴포넌트 토글</button>
-      {mounted && <Counter />}
-    </>
-  );
-};
+//? useEffect 구현현
+{
+  // import React from "react";
+  // import MyReact from "./lib/MyReact";
+  // const Counter = () => {
+  //   MyReact.resetCursor();
+  //   const [count, setCount] = React.useState(0);
+  //   const [name, setName] = React.useState("");
+  //   const handleClick = () => setCount(count + 1);
+  //   const handleChangeName = (e) => {
+  //     setName(e.target.value);
+  //   };
+  //   MyReact.useEffect(() => {
+  //     document.title = `count: ${count} | name: ${name}`;
+  //     console.log("effect1");
+  //     return function cleanup() {
+  //       document.title = "";
+  //       console.log("effect1 cleanup");
+  //     };
+  //   }, [count, name]);
+  //   MyReact.useEffect(() => {
+  //     localStorage.setItem("name", name);
+  //     console.log("effect2");
+  //   }, [name]);
+  //   MyReact.useEffect(() => {
+  //     setName(localStorage.getItem("name") || "");
+  //     console.log("effect3");
+  //   }, []);
+  //   console.log("Counter rendered");
+  //   return (
+  //     <>
+  //       <button onClick={handleClick}>더하기</button>
+  //       <input value={name} onChange={handleChangeName} />
+  //     </>
+  //   );
+  // };
+  // export default () => {
+  //   const [mounted, setMounted] = React.useState(false);
+  //   const handleToggle = () => {
+  //     const nextMounted = !mounted;
+  //     if (!nextMounted) MyReact.cleanupEffects();
+  //     setMounted(nextMounted);
+  //   };
+  //   return (
+  //     <>
+  //       <button onClick={handleToggle}>컴포넌트 토글</button>
+  //       {mounted && <Counter />}
+  //     </>
+  //   );
+  // };
+}
--- lecture/[리액트 2부] 고급 주제와 훅(by 김정환)/2-hook/src/pages/ProductPage/index.jsx
+++ lecture/[리액트 2부] 고급 주제와 훅(by 김정환)/2-hook/src/pages/ProductPage/index.jsx
@@ -1,3 +1,39 @@
-const ProductPage = () => <>ProductPage</>;
+import React, { useEffect } from "react";
+import OrderableProductItem from "./OrderableProductItem";
+import Page from "../../components/Page";
+import Title from "../../components/Title";
+import Navbar from "../../components/Navbar";
+import ProductApi from "shared/api/ProductApi";
+
+const ProductPage = () => {
+  const [productList, setProductList] = React.useState([]);
+
+  const fetch = async () => {
+    try {
+      const productList = await ProductApi.fetchProductList();
+      setProductList(productList);
+    } catch (e) {
+      return;
+    }
+  };
+
+  useEffect(() => {
+    fetch();
+  }, []);
+
+  return (
+    <div className="ProductPage">
+      <Page header={<Title>메뉴목록</Title>} footer={<Navbar />}>
+        <ul>
+          {productList.map((product) => (
+            <li key={product.id}>
+              <OrderableProductItem product={product} />
+            </li>
+          ))}
+        </ul>
+      </Page>
+    </div>
+  );
+};
 
 export default ProductPage;
