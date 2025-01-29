350e3885f65452e9818ee0bef96853f77cbf043a
황상윤 <s_infinite@naver.com>
2025-01-29 23:48:27 +0900

    [feat] MyLayout 함수화 (2)

--- lecture/[리액트 2부] 고급 주제와 훅(by 김정환)/2-hook/src/lib/MyLayout.jsx
+++ lecture/[리액트 2부] 고급 주제와 훅(by 김정환)/2-hook/src/lib/MyLayout.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import Backdrop from "../components/Backdrop";
+import Dialog from "../components/Dialog";
 
 export const layoutContext = React.createContext({});
 layoutContext.displayName = "LayoutContext";
@@ -6,12 +9,40 @@ layoutContext.displayName = "LayoutContext";
 export const Layout = ({ children }) => {
   const [dialog, setDialog] = React.useState();
 
-  const value = {
-    dialog: dialog,
-    setDialog: setDialog,
-  };
+  return (
+    <layoutContext.Provider value={{ dialog, setDialog }}>
+      {children}
+    </layoutContext.Provider>
+  );
+};
+
+export const useDialog = () => {
+  const { dialog, setDialog } = React.useContext(layoutContext);
+
+  const openDialog = (element) => setDialog(element);
+  const closeDialog = () => setDialog(null);
+
+  return { dialog, openDialog, closeDialog };
+};
+
+export const useLoading = () => {
+  const { openDialog, closeDialog: finishLoading } = useLoading();
+
+  const startLoading = (message) => openDialog(<Dialog>{message}</Dialog>);
+
+  return { startLoading, finishLoading };
+};
+
+export const DialogContainer = () => {
+  const { dialog } = useDialog();
 
   return (
-    <layoutContext.Provider value={value}>{children}</layoutContext.Provider>
+    <>
+      {dialog &&
+        ReactDOM.createPortal(
+          <Backdrop>{dialog}</Backdrop>,
+          document.querySelector("#dialog")
+        )}
+    </>
   );
 };
