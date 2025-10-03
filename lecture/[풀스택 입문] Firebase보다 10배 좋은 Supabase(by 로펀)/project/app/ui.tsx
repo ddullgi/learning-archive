bbe00f91af14e2c78e76a2bea888a0f039747483
황상윤 <s_infinite@naver.com>
2025-10-03 22:45:54 +0900

    [study] supabase 미니노트 프로젝트 UI 완성

--- lecture/[풀스택 입문] Firebase보다 10배 좋은 Supabase(by 로펀)/project/app/ui.tsx
+++ lecture/[풀스택 입문] Firebase보다 10배 좋은 Supabase(by 로펀)/project/app/ui.tsx
@@ -1,26 +1,35 @@
 "use client"
 
+import EmptyNote from "@/components/empty-note"
 import Header from "@/components/header"
 import NewNote from "@/components/new-note"
+import NoteViewer from "@/components/note-viewer"
 import Sidebar from "@/components/sidebar"
+import { useState } from "react"
 
 const notes = [
   { id: 1, title: "첫 번째 노트", content: "노트 내용 1" },
   { id: 2, title: "두 번째 노트", content: "노트 내용 2" },
-  { id: 3, title: "세 번째 노트", content: "노트 내용 3" },
-  { id: 4, title: "네 번째 노트", content: "노트 내용 4" },
 ]
 
 export default function UI() {
+  const [activeNoteId, setActiveNoteId] = useState(null)
+  const [isCreating, setIsCreating] = useState(false)
+
   return (
     <main className="w-full h-screen flex flex-col">
       <Header />
       <div className="grow relative">
-        <Sidebar notes={notes} />
-        {/* New Note */}
-        <NewNote />
-        {/* Note Viewer (Edit or View)*/}
-        {/* Empty Note */}
+        <Sidebar
+          notes={notes}
+          activeNoteId={activeNoteId}
+          setActiveNoteId={setActiveNoteId}
+          setIsCreating={setIsCreating}
+        />
+        {
+          isCreating ? <NewNote setIsCreating={setIsCreating} />
+            : activeNoteId ? <NoteViewer note={notes.find((note) => note.id === activeNoteId)} /> : <EmptyNote />
+        }
       </div>
     </main>
   )
--- lecture/[풀스택 입문] Firebase보다 10배 좋은 Supabase(by 로펀)/project/components/sidebar.tsx
+++ lecture/[풀스택 입문] Firebase보다 10배 좋은 Supabase(by 로펀)/project/components/sidebar.tsx
@@ -1,15 +1,37 @@
 "use client"
 
-export default function Sidebar({ notes }) {
+export default function Sidebar({
+  activeNoteId,
+  setActiveNoteId,
+  setIsCreating,
+  notes
+}) {
   return (
-    <aside className="absolute top-0 bottom-0 left-0 w-1/3  p-2">
-      <button type="button" className="p-2 text-lg font-bold border border-gary-600 rounded-lg w-full">
+    <aside className="absolute top-0 bottom-0 left-0 w-1/3 p-2 border-r border-gray-300">
+      <button
+        onClick={() => {
+          setIsCreating(true)
+          setActiveNoteId(null)
+        }}
+        type="button"
+        className="p-2 text-lg font-bold border border-gray-600 rounded-lg w-full">
         + 새로운 노트
       </button>
 
       <ul className="mt-2 flex flex-col gap-2">
         {notes.map((note) =>
-          <li key={note.id}>{note.title}</li>
+          <li key={note.id}>
+            <button
+              className={`${activeNoteId === note.id ? "font-bold" : ""}`}
+              onClick={() => {
+                setActiveNoteId(note.id)
+                setIsCreating(false)
+              }}
+              type="button"
+            >
+              {note.title}
+            </button>
+          </li>
         )}
       </ul>
     </aside>
